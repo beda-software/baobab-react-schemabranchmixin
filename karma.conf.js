73ab14a6e9355c919bd8116f933e9a30837e89d9
ruscoder <rusmiligamer@gmail.com>
2016-01-15 12:05:33 +0700

    Refactored config

--- karma.conf.js
+++ karma.conf.js
@@ -1,37 +1,28 @@
-var path = require('path');
-var webpack = require('webpack');
+var babelOptions = {
+  presets: ['es2015', 'react']
+};
 
 module.exports = function (config) {
   config.set({
     basePath: './',
     frameworks: ['phantomjs-shim', 'mocha', 'chai'],
+
     files: [
       'test/*.spec.js'
     ],
     preprocessors: {
       'test/*.spec.js': 'webpack'
     },
+
     reporters: ['coverage', 'dots'],
-    notifyReporter: {
-      reportEachFailure: true
-    },
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['PhantomJS'],
-    singleRun: false,
+
     webpack: {
-      babel: {
-        presets: ['es2015', 'react']
-      },
+      babel: babelOptions,
       isparta: {
         embedSource: true,
         noAutoWrap: true,
         // these babel options will be passed only to isparta and not to babel-loader
-        babel: {
-          presets: ['es2015', 'react']
-        }
+        babel: babelOptions
       },
       module: {
         preLoaders: [
@@ -41,20 +32,27 @@ module.exports = function (config) {
             loader: 'babel',
             exclude: /node_modules/
           },
-          // Transpile all project without tests and index.js
+          // Transpile all project without tests
           {
             test: /\.jsx?$/,
+            loader: 'isparta',
             exclude: [
               /node_modules/,
               /\.spec\.jsx?/
-            ],
-            loader: 'isparta'
+            ]
           }
         ]
       }
     },
     webpackServer: {
       noInfo: true
-    }
+    },
+
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    browsers: ['PhantomJS'],
+    autoWatch: true,
+    singleRun: false
   });
 };
